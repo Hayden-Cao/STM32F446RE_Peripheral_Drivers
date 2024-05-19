c2affd525f9d23f7e3f5344e40e1a723f62f9ae5
Hayden Cao <130268332+pileofhay@users.noreply.github.com>
2024-05-19 09:21:54 -0700

    Update stm32f446xx_GPIO_Driver.h

--- Inc/stm32f446xx_GPIO_Driver.h
+++ Inc/stm32f446xx_GPIO_Driver.h
@@ -15,19 +15,19 @@
  */
 typedef struct
 {
-	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;			/*!< possible values from @GPIO_PIN_MODES >*/
-	uint8_t GPIO_PinSpeed;			/*!< possible values from @GPIO_PIN_SPEED >*/
-	uint8_t GPIO_PinPuPdControl;
-	uint8_t GPIO_PinOPType;
-	uint8_t GPIO_PinAltFunMode;
+	uint8_t GPIO_PinNumber;			/* Holds what Pin to be configured      */
+	uint8_t GPIO_PinMode;			/* possible values from @GPIO_PIN_MODES */
+	uint8_t GPIO_PinSpeed;			/* possible values from @GPIO_PIN_SPEED */
+	uint8_t GPIO_PinPuPdControl;		/* Pull Up / Pull Down / None Control 	*/
+	uint8_t GPIO_PinOPType;			/* Pin output type Control		*/
+	uint8_t GPIO_PinAltFunMode;		/* Alternate Function Mode for GPIO	*/
 
 }	GPIO_PinConfig_t;
 
 typedef struct
 {
-	GPIO_RegDef_t *pGPIOx;       		/*!< This holds the base address of the GPIO port to which the pin belongs >*/
-	GPIO_PinConfig_t GPIO_PinConfig;   /*!< This holds GPIO pin configuration settings >*/
+	GPIO_RegDef_t *pGPIOx;       		/* This holds the base address of the GPIO port to which the pin belongs */
+	GPIO_PinConfig_t GPIO_PinConfig;   	/* This holds GPIO pin configuration settings */
 
 }	GPIO_Handle_t;
 
