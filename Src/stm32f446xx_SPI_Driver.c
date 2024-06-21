39282e864c6974e4c0504a65020ffede17455161
Hayden Cao <130268332+Hayden-Cao@users.noreply.github.com>
2024-06-20 19:20:17 -0700

    Add files via upload

--- Src/stm32f446xx_SPI_Driver.c
+++ Src/stm32f446xx_SPI_Driver.c
@@ -55,7 +55,57 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 
 // Init and Deinit
 
-void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+
+	//peripheral clock enable
+
+	SPI_PeriClockControl(pSPIHandle->pSPIx, ENABLE);
+
+	//first lets configure the SPI_CR1 register
+
+	uint32_t tempreg = 0;
+
+	//1. configure the device mode
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR ;
+
+	//2. Configure the bus config
+	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		//bidi mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE);
+
+	}
+	else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		//bidi mode should be set
+		tempreg |= ( 1 << SPI_CR1_BIDIMODE);
+	}
+	else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		//BIDI mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE);
+		//RXONLY bit must be set
+		tempreg |= ( 1 << SPI_CR1_RXONLY);
+	}
+
+	// 3. Configure the spi serial clock speed (baud rate)
+	tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR;
+
+	//4.  Configure the DFF
+	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF;
+
+	//5. configure the CPOL
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
+
+	//6 . configure the CPHA
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+
+	tempreg |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
+
+	pSPIHandle->pSPIx->CR1 = tempreg;
+
+}
 void SPI_DeInit(SPI_RegDef_t *pSPIx);
 
 // Data send and receive
