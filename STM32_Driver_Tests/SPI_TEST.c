deca9f159f8dee05e4cbdf6d3818157d160f3bd6
Hayden Cao <130268332+Hayden-Cao@users.noreply.github.com>
2024-06-23 21:50:04 -0700

    Update SPI_TEST.c

--- STM32_Driver_Tests/SPI_TEST.c
+++ STM32_Driver_Tests/SPI_TEST.c
@@ -14,15 +14,12 @@ void SPI2_Pin_Init()
 
     SPI_Pins.pGPIOx = GPIOB;
     SPI_Pins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
-    SPI_Pins.GPIO_PinConfig.GPIO_PinAltFunMode = GPIO_MODE_IT_FT;
+    SPI_Pins.GPIO_PinConfig.GPIO_PinAltFunMode = GPIO_MODE_IT_RT;
     SPI_Pins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
     SPI_Pins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
     SPI_Pins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 
     SPI_Pins.GPIO_PinConfig.GPIO_PinNumber = 15;
-    GPIO_Init(&SPI_Pins);
-
-    SPI_Pins.GPIO_PinConfig.GPIO_PinNumber = 14;
     GPIO_Init(&SPI_Pins);
 
     SPI_Pins.GPIO_PinConfig.GPIO_PinNumber = 13;
@@ -41,37 +38,25 @@ void SPI2_Init()
     SPI_NO_2.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
     SPI_NO_2.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
     SPI_NO_2.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV2;
-    SPI_NO_2.SPIConfig.SPI_SSM = SPI_SSM_DI;
+    SPI_NO_2.SPIConfig.SPI_SSM = SPI_SSM_EN;
 
     SPI_Init(&SPI_NO_2);
 
 }
 
-void Testing_Delay()
-{
-    for(int i = 0; i < 50000; i++)
-    {
-        
-    }
-
-}
-
-
 int main (void)
-{  
+{
     SPI2_Pin_Init();
     SPI2_Init();
+    SPI_SSIControl(SPI2, ENABLE);
     SPI_PeripheralControl(SPI2, ENABLE);
-    uint8_t buf[16];
+    char buf[] = "Hello World";
 
-    strncpy((char *)buf, "Hello World!\r\n", sizeof(buf) - 1);
-
-    for(;;)
+    while(1)
     {
-        SPI_SendData(SPI2, &buf, 2);
-        Testing_Delay();
+    	while(SPI_GetFlagStatus(SPI2, SPI_BUSY_FLAG));
+    	SPI_SendData(SPI2, (uint8_t*)buf, strlen(buf));
     }
-
     return 0;
 }
 
